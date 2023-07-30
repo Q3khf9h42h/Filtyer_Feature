d1ce86b47824502bc8aad0fa7f86121ce75927a8
harshita sethiya <harshita.sethiya252@gmail.com>
2023-07-31 02:00:42 +0530

    demo

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import React,{useState} from 'react';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+import Card from '../UI/Card';
+const Expenses=(props)=>{
+    const [filteredYear,setFilteredYear]=useState('2020');
+
+    const filterChangeHandler=selectedYear=>{
+        setFilteredYear(selectedYear);
+    };
+    return(
+        <div>
+        <Card className="expenses">
+    <expenseFilter 
+    selected={filteredYear} 
+    onChangeFilter={filterChangeHandler}
+    />
+    {props.items.map((expense) =>(
+        <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        />
+
+    ))}
+    
+     </Card>
+     </div>
+
+    );
+};
+export default Expenses;
