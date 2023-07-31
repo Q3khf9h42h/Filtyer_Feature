670be9cecc242483385a723a88f71017aff95150
harshita sethiya <harshita.sethiya252@gmail.com>
2023-07-31 09:47:19 +0530

    demo2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,6 +8,9 @@ const Expenses=(props)=>{
     const filterChangeHandler=selectedYear=>{
         setFilteredYear(selectedYear);
     };
+    const filteredExpenses=props.items.filter(expense =>{
+        return expense.date.getFullYear().toString()===filteredYear;
+    });
     return(
         <div>
         <Card className="expenses">
